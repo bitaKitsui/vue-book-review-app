709be70622603745a6db3c4795578b04685eb3c7
bitakitsui <bitakitsui@bitakitsui.local>
2022-01-13 18:00:18 +0900

    feat: APIの検索結果をstateへ保存

---
+++ src/api/default.ts
@@ -0,0 +1,7 @@
+import {Result} from "../types/types";
+
+export const defaultResult: Result = {
+    items: [],
+    kind: '',
+    totalItems: 0
+}
--- src/components/pages/BookMainPage.vue
+++ src/components/pages/BookMainPage.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import {reactive} from "vue";
 import BookMainTemplate from "../templates/BookMainTemplate.vue";
 import {searchBook} from "../../api";
+import {defaultResult} from "../../api/default";
+
+const state = reactive({ result: defaultResult })
 
 const onSubmit = async (query: string) => {
-  const data = await searchBook(query)
-  console.log('data', data)
+  state.result = await searchBook(query)
 }
+
 </script>
 
 <template>
