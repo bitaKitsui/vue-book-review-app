368f17c76e6120beb5e14750dbc13d20e264c611
bitakitsui <bitakitsui@bitakitsui.local>
2022-01-14 17:57:43 +0900

    fix: fetchBookByIdの返り値に型定義

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-import {Result} from "../types/types";
+import { Result, BookItem } from "../types/types";
 
 export const searchBook = async (query: string): Promise<Result> => {
     const { data } = await axios.get<Result>(`/books/v1/volumes?q=${query}`)
     return data
 }
 
-export const fetchBookById = async (id: string): Promise<any> => {
-    const { data } = await axios.get<any>(`/books/v1/volumes/${id}`)
+export const fetchBookById = async (id: string): Promise<BookItem> => {
+    const { data } = await axios.get<BookItem>(`/books/v1/volumes/${id}`)
     return data
 }
